70f178f4d597e6df21aac3f806017440f613f584
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-12-10 16:23:03 +0900

    fix

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function install(Vue: any, options: any, mode: any, lines: any): void;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
-require('vue').use(require('./ace-directive'), true, 'ruby');
+import ace from 'brace';
+
+exports.install = (Vue, options, mode, lines) => {
+  Vue.directive("ace", {
+    twoWay: true,
+    bind: function () {
+      this.editor = ace.edit(this.el);
+      this.editor.setTheme("ace/theme/github");
+      this.editor.getSession().setMode("ace/mode/" + mode);
+      this.editor.getSession().setUseWrapMode(true);
+      this.editor.$blockScrolling = Infinity;
+
+      if(!lines) {
+        lines = Infinity;
+      }
+
+      this.editor.setOptions({
+        maxLines: lines,
+        minLines: 15,
+      });
+
+      if (options) {
+        this.editor.setOptions({
+          readOnly: options,
+          highlightActiveLine: false,
+          highlightGutterLine: false
+        });
+        this.editor.renderer.$cursorLayer.element.style.opacity = 0;
+      }
+
+      this.silent = false;
+      this.handler = () => {
+        if (!this.silent) {
+          this.set(this.editor.getSession().getValue(), true);
+        }
+      };
+      this.editor.on("change", this.handler);
+    },
+    update: function (value, oldValue) {
+      this.silent = true;
+      this.editor.getSession().setValue(value);
+      this.silent = false;
+    }
+  });
+};
